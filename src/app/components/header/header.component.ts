b15538e8557c1b1ae9644c430e1131e12f7e7e03
Hari Haran <iamhariharan8@gmail.com>
2023-05-12 17:15:51 +0530

    Todolist app is added to github

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,64 @@
+// import { Component, OnInit } from '@angular/core';
+// import { UiService } from '../../services/ui.service';
+// import { Subscription } from 'rxjs';
+
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.css']
+// })
+// export class HeaderComponent implements OnInit{
+
+//   title : string = "To do list";
+//   showAddTask: boolean = false;
+//   subscription!:Subscription;
+
+//   constructor(private uiService: UiService) {
+//     this.subscription = this.uiService
+//       .onToggle()
+//       .subscribe((value) => (this.showAddTask = value));
+//   }
+//   ngOnInit():void {}
+
+//   toggleAddTask(){
+//     this.uiService.toggleAddTask();
+//   }
+
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+// import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {}
+  
+  //  ngOnDestroy() {
+  //    // Unsubscribe to ensure no memory leaks
+  //    this.subscription.unsubscribe();
+  //  }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+  // hasRoute(route: string) {
+  //   return this.router.url === route;
+  // }
+}
